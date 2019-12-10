73a2a8b1e8fe6e71320db61e7a88d4f655add207
Thomas Mendoza <thomas@hitotsu.me>
2019-12-10 10:51:40 -0800

    Find python3 rather than provide an explicit path

--- gitlab_runner_config.py
+++ gitlab_runner_config.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
 ###############################################################################
 # Copyright (c) 2019, Lawrence Livermore National Security, LLC
